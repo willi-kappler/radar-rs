b6c26be376ac469be3eb8f66b9a839d2a71ac6d4
Willi Kappler <grandor@gmx.de>
2016-12-15 14:56:46 +0100

    initial release

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+//! radar-rs: processing radar data with Rust
+//!
+//! Written by Willi Kappler, Version 0.1 (2016.12.15)
+//!
+//! Repository: https://github.com/willi-kappler/radar-rs
+//!
+//! License: MIT
+//!
+
+// For clippy
+// #![feature(plugin)]
+//
+// #![plugin(clippy)]
+
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
+
+#[macro_use] extern crate log;
+extern crate jobsteal;
+#[macro_use] extern crate error_chain;
